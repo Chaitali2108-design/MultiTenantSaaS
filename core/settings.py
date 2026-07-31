23438d18e412ca6b013b08ede10842926cfe5ced
Chaitali Chandele <chaitalichandele2108@gmail.com>
2026-07-31 14:19:56 +0530

    Fix template directory configuration

--- core/settings.py
+++ core/settings.py
@@ -57,7 +57,8 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR /'projects' /'templates'],
+        'DIRS': [BASE_DIR / 'templates',
+            BASE_DIR / 'projects' / 'templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
