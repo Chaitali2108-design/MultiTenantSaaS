3ae5258475775deef9c60571b10dee072ecb0d4a
Chaitali Chandele <chaitalichandele2108@gmail.com>
2026-08-05 20:23:14 +0530

    Member1: Public pages, login UI, feature pages and navigation updates

--- corepages/urls.py
+++ corepages/urls.py
@@ -8,4 +8,34 @@ urlpatterns = [
         views.features,
         name="features"
     ),
+
+    path(
+    "features/organization-management/",
+    views.organization_management,
+    name="organization_management"
+),
+
+path(
+    "features/user-management/",
+    views.user_management,
+    name="user_management"
+),
+
+path(
+    "features/role-based-access/",
+    views.role_based_access,
+    name="role_based_access"
+),
+
+path(
+    "features/project-management/",
+    views.project_management,
+    name="project_management"
+),
+
+path(
+    "features/reports-analytics/",
+    views.reports_analytics,
+    name="reports_analytics"
+),
 ]
--- corepages/views.py
+++ corepages/views.py
@@ -14,3 +14,55 @@ def features(request):
         request,
         "corepages/features.html"
     )
+
+
+
+from django.shortcuts import render
+
+from organizations.models import Organization
+from accounts.models import User
+from projects.models import Project, Task
+
+
+def organization_management(request):
+
+    context = {
+        "organization_count": Organization.objects.count(),
+        "user_count": User.objects.count(),
+        "project_count": Project.objects.count(),
+        "task_count": Task.objects.count(),
+    }
+
+    return render(
+        request,
+        "corepages/organization_management.html",
+        context,
+    )
+
+
+def user_management(request):
+    return render(
+        request,
+        "corepages/user_management.html"
+    )
+
+
+def role_based_access(request):
+    return render(
+        request,
+        "corepages/role_based_access.html"
+    )
+
+
+def project_management(request):
+    return render(
+        request,
+        "corepages/project_management.html"
+    )
+
+
+def reports_analytics(request):
+    return render(
+        request,
+        "corepages/reports_analytics.html"
+    )
